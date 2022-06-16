a0b8e2b1f1542109def878df7c6f3859a473244e
ahmadijmail <ahmadijmil2010@gmail.com>
2022-06-16 06:03:00 +0300

    Class07

---
+++ __test__/middleware/basic-auth-middleware.test.js
@@ -0,0 +1,58 @@
+"use strict";
+
+process.env.API_SECRET = "SECRET";
+
+const base64 = require("base-64");
+const middleware = require("../../src/auth/middleware/basic.js");
+const { db, users } = require("../../src/auth/models/index.js");
+
+let userInfo = {
+  admin: { username: "admin-basic", password: "password" },
+};
+beforeAll(async () => {
+  await db.sync();
+  await users.create(userInfo.admin);
+});
+afterAll(async () => {
+  await db.drop();
+});
+
+describe("Auth Middleware", () => {
+  const req = {};
+  const res = {
+    status: jest.fn(() => res),
+    send: jest.fn(() => res),
+  };
+  const next = jest.fn();
+
+  describe("user authentication", () => {
+    it("fails a login for a user (admin) with the incorrect basic credentials", () => {
+      const basicAuthString = base64.encode("username:password");
+
+      // Change the request to match this test case
+      req.headers = {
+        authorization: `Basic ${basicAuthString}`,
+      };
+
+      return middleware(req, res, next).then(() => {
+        expect(next).not.toHaveBeenCalled();
+        expect(res.status).toHaveBeenCalledWith(403);
+      });
+    });
+
+    it("logs in an admin user with the right credentials", () => {
+      let basicAuthString = base64.encode(
+        `${userInfo.admin.username}:${userInfo.admin.password}`
+      );
+
+      // Change the request to match this test case
+      req.headers = {
+        authorization: `Basic ${basicAuthString}`,
+      };
+
+      return middleware(req, res, next).then(() => {
+        expect(next).not.toHaveBeenCalledWith();
+      });
+    });
+  });
+});
---
+++ src/auth/router/handlers.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const { users } = require("../models/index.js");
+
+async function handleSignup(req, res, next) {
+  try {
+    users
+      .beforeCreate(req.body.password)
+      .then(async (hashedPass) => {
+        let userRecord = await users.create({
+          username: req.body.username,
+          password: hashedPass,
+        });
+
+        const output = {
+          user: userRecord,
+          token: userRecord.token,
+        };
+
+        res.status(201).json(userRecord);
+      })
+      .catch((e) => {});
+  } catch (e) {
+    
+    next(e);
+  }
+}
+
+async function handleSignin(req, res, next) {
+  try {
+    const user = {
+      user: req.user,
+      token: req.user.token,
+    };
+
+    res.status(200).json(user);
+  } catch (e) {
+    next(e);
+  }
+}
+
+async function handleGetUsers(req, res, next) {
+  try {
+    let come = req.user;
+    res.status(200).json(req.user);
+  } catch (e) {
+    
+    next(e);
+  }
+}
+
+function handleSecret(req, res, next) {
+  res.status(200).send("Welcome to the secret area!");
+}
+
+module.exports = {
+  handleSignup,
+  handleSignin,
+  handleGetUsers,
+  handleSecret,
+};
